76b0ec9f66ae82f59d17510fd6c33b761de9211e
John Lira <johnalsoares@gmail.com>
2026-01-08 12:50:13 -0300

    feat: integrate CORS middleware and update dependencies for improved API security

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import apiRouter from "./routes/api";
 import { errorHandler } from "./errors/error-handler";
 import logger from "./config/logger";
@@ -7,8 +8,10 @@ import { env } from "./config/env";
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(logger);
 app.use(express.json());
+app.use(express.static("public"));
 app.use("/api", apiRouter);
 app.use(errorHandler);
 
